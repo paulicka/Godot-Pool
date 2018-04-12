2e3e9a760120ba6c26b3d033c5f65abaae88d374
Mr-Slurpy <danieleliasinski@msn.com>
2018-04-12 03:45:31 -0400

    stuff

--- ball/ball/ball.gd
+++ ball/ball/ball.gd
@@ -1,18 +1,5 @@
 extends Node
 
-enum Ball{
-	SOLID_1, SOLID_2, SOLID_3,
-	SOLID_4, SOLID_5, SOLID_6,
-	SOLID_7, BALL_8, STRIPE_9,
-	STRIPE_10, STRIPE_11, STRIPE_12,
-	STRIPE_13, STRIPE_14, STRIPE_15,
-	CUE_BALL
-}
-
-const styles = [
-	"default"
-]
-
 var current_style = 0
 
 func _ready():
---
+++ game.gd
@@ -0,0 +1,30 @@
+extends Node
+
+const BALL_SIZE = 1.125
+
+enum Ball{
+	SOLID_1, SOLID_2, SOLID_3,
+	SOLID_4, SOLID_5, SOLID_6,
+	SOLID_7, BALL_8, STRIPE_9,
+	STRIPE_10, STRIPE_11, STRIPE_12,
+	STRIPE_13, STRIPE_14, STRIPE_15,
+	CUE_BALL
+}
+
+const ball_styles = [
+	"default"
+]
+
+const table_styles = [
+	
+]
+
+const cue_styles = [
+	
+]
+
+func _ready():
+	pass
+
+func _process(delta):
+	
