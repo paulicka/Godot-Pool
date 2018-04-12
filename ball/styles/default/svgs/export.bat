468b3c5fd8282fbe0b7aa7e96d94b9880ec0b8d8
Daniel Eliasinski <danieleliasinski@msn.com>
2018-04-12 17:07:29 -0400

    Added png generator!

--- ball/styles/default/svgs/export.bat
+++ ball/styles/default/svgs/export.bat
@@ -1,5 +1,11 @@
 @echo off
-set "AllowExt=.jpg .png .bmp"
-for %%a in (%AllowExt%) do (
-	forfiles /p ..\ /m *%%a /c "cmd /c DEL @path"
+
+rem Clean old images
+echo Cleaning old files...
+forfiles /p ..\ /m "*.png" /c "cmd /c DEL @path"
+
+rem create new images
+FOR %%f IN (*.svg) DO (
+	echo Exporting %%f
+	"C:\Program Files\Inkscape\inkscape.exe" -z "%%~nf.svg" -e "../%%~nf.png"
 )
